81d615fd15ab64f0b008abd7d3db8e1fc0798a7e
asbyvshev <barseto4ka@live.ru>
2020-03-23 12:23:06 +0300

    обработка ошибки 500

--- src/main/java/ru/geekbrains/myshop/controllers/ShopExceptionHandler.java
+++ src/main/java/ru/geekbrains/myshop/controllers/ShopExceptionHandler.java
@@ -5,19 +5,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import ru.geekbrains.myshop.exceptions.InternalServiceErrorExeption;
 import ru.geekbrains.myshop.exceptions.ProductNotFoundException;
 
 @Slf4j
 @ControllerAdvice
 public class ShopExceptionHandler {
 
-    // TODO ДЗ - оформить страницу ошибки 404
-
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ProductNotFoundException.class)
     public String handleProductNotFoundException(final ProductNotFoundException ex) {
         log.error("Product not found thrown", ex);
         return "error";
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(InternalServiceErrorExeption.class)
+    public String handleInternalServiceErrorExeption(final InternalServiceErrorExeption ex) {
+        log.error("500 Internal Service Error thrown", ex);
+        return "error500";
     }
 
 }
---
+++ src/main/java/ru/geekbrains/myshop/exceptions/InternalServiceErrorExeption.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.myshop.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+public class InternalServiceErrorExeption extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public InternalServiceErrorExeption(String message) {
+        super(message);
+    }
+}
