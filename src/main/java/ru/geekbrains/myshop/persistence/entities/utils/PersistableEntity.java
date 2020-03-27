b82720789457a192b84f52396b14e8a0d97c23a8
asbyvshev <barseto4ka@live.ru>
2020-03-27 13:09:17 +0300

    указаны пути - мас

--- src/main/java/ru/geekbrains/myshop/persistence/entities/utils/PersistableEntity.java
+++ src/main/java/ru/geekbrains/myshop/persistence/entities/utils/PersistableEntity.java
@@ -2,6 +2,8 @@ package ru.geekbrains.myshop.persistence.entities.utils;
 
 import lombok.Data;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
@@ -12,6 +14,6 @@ import java.util.UUID;
 public abstract class PersistableEntity implements Serializable {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
 }
