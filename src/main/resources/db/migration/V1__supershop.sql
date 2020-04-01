ed556bc08b602a67808e5edfb3df332ee00422f6
asbyvshev <barseto4ka@live.ru>
2020-04-01 16:50:46 +0300

    добавлены новые скрипты

--- src/main/resources/db/migration/V1__supershop.sql
+++ src/main/resources/db/migration/V1__supershop.sql
@@ -88,8 +88,9 @@
 
 
 -- -- Srcipts for MySqL
-
- CREATE DATABASE IF NOT EXISTS `geek_shop` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+-- настройка timeZone
+-- set global time_zone = '-3:00';
+-- CREATE DATABASE IF NOT EXISTS `geek_shop` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 use `geek_shop`;
 
