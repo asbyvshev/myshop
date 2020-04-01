4d845f19277f2096ee1064e392ddca31e70394a3
asbyvshev <barseto4ka@live.ru>
2020-04-01 16:57:36 +0300

    добавление картики

--- src/main/java/ru/geekbrains/myshop/controllers/ReviewController.java
+++ src/main/java/ru/geekbrains/myshop/controllers/ReviewController.java
@@ -2,26 +2,43 @@ package ru.geekbrains.myshop.controllers;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import ru.geekbrains.myshop.services.ImageService;
 import ru.geekbrains.myshop.services.ReviewService;
 
+import javax.imageio.ImageIO;
 import javax.persistence.EntityNotFoundException;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/reviews")
 @RequiredArgsConstructor
 public class ReviewController {
     private final ReviewService reviewService;
+    private final ImageService imageService;
 
     @GetMapping("/{id}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String moderateReview(@PathVariable Long id, @RequestParam String option) throws EntityNotFoundException {
         return "redirect:/products/" + reviewService.moderate(id, option);
+    }
+
+    @GetMapping(value = "/images/{id}", produces = MediaType.IMAGE_PNG_VALUE)
+    public @ResponseBody
+    byte[] getImage(@PathVariable String id) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        BufferedImage bufferedImage = imageService.loadFile(id);
+        if (bufferedImage != null) {
+            ImageIO.write(bufferedImage,"png", byteArrayOutputStream);
+            return byteArrayOutputStream.toByteArray();
+        } else {
+            return new byte[0];
+        }
     }
 }
